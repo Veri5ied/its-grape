9f67d1ee7bf57072fcd1d825366cd17f50e7eba6
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2021-11-16 13:02:55 +0100

    roles update

--- src/pages/roles/Roles.js
+++ src/pages/roles/Roles.js
@@ -1,17 +1,15 @@
 import Navbar from "../../components/navbar/Navbar";
-import "./Roles.css"
+import "./Roles.css";
+import Loading from "../../components/loading/Loading";
 
 function Roles() {
   return (
     <div className="roles">
       <Navbar />
       <div className="roles_body container mt-4">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-          tempore fugit ipsa eos reprehenderit, dolore aliquam nulla quibusdam
-          porro libero cupiditate unde molestias. Repellendus ipsam dolorem, eos
-          sed tenetur itaque.
-        </p>
+        <h3>Coming Soon !!!</h3>
+        <hr />
+        <Loading />
       </div>
     </div>
   );
